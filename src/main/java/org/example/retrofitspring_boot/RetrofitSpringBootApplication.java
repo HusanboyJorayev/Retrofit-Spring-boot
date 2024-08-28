8636b5f7275b26f5f708f9011950895d41c7d206
HusanboyJorayev <144492116+HusanboyJorayev@users.noreply.github.com>
2024-08-28 11:41:17 +0500

    Added Retrofit

---
+++ src/main/java/org/example/retrofitspring_boot/RetrofitSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.retrofitspring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrofitSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetrofitSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/retrofitspring_boot/RetrofitSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.retrofitspring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrofitSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
